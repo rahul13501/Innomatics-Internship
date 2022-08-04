384d2165ac7bb2cf79ed3a44a8ac3b1d65d35291
rahul13501 <99610008+rahul13501@users.noreply.github.com>
2022-08-05 01:59:47 +0530

    Add files via upload

---
+++ Python Tasks/task 4/2.py
@@ -0,0 +1,12 @@
+#Q.2.String split and Join
+def split_and_join(line):
+    # write your code here
+    a = line.split(" ") 
+    b= "-".join(a)
+    return b
+
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
---
+++ Python Tasks/task 4/3.py
@@ -0,0 +1,9 @@
+#Q.3.What's Your name?
+def print_full_name(first, last):
+    # Write your code here
+    last = last+"!"
+    print("Hello",first, last,"You just delved into python.");
+    
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
