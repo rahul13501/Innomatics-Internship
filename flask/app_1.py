29a9012f9a0d991ab8ee5ad17681d48da5c82716
rahul13501 <99610008+rahul13501@users.noreply.github.com>
2022-08-10 00:09:07 +0530

    Add files via upload

---
+++ flask/app_1.py
@@ -0,0 +1,15 @@
+# Step 1 - import
+from flask import Flask
+
+# Step 2 - Create the object
+app = Flask(__name__)
+
+#Step 3 - Define the Routes and bind it with functions
+@app.route('/')
+def index():
+    return "Welcome to Rahul's Application"
+
+
+# Step 4 - Run the app
+if __name__== '__main__':
+    app.run()
